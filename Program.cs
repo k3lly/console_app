f7f711f410589a210161934ed1cc76c45f435544
fu <fu@email.com>
2013-01-30 06:40:35 +0700

    olo

--- Program.cs
+++ Program.cs
@@ -12,6 +12,7 @@ namespace console_app
             Console.WriteLine("what's your name?");
             var name = Console.ReadLine();
             Console.WriteLine("Fuck you, " + name);
+            Console.WriteLine("olo");
             Console.ReadLine();
         }
     }
