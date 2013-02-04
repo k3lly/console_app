05fb36894518afdcf636bc15065f6576a972e2a0
rockytoy <oc211088@hotmail.com>
2013-02-04 16:44:05 +0700

    mass edit

--- Program.cs
+++ Program.cs
@@ -1,36 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace console_app
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-
-            Console.WriteLine("what's your name?");
-            var name = Console.ReadLine();
-            Console.WriteLine("Hello, " + name);
-			Console.WriteLine("add na ja");
-			Console.WriteLine("add na ja2");
-			Console.WriteLine("add na ja3");
-            Console.WriteLine("Fuck you, " + name);
-            Console.WriteLine("olo");
-            Console.WriteLine("Fuck you");
-            Console.WriteLine("damn");
-			ton
-            Console.WriteLine("Very much");
-            Console.WriteLine("dev");
-            Console.ReadLine();
-			master
-            Console.WriteLine("hello world");
-            Console.WriteLine("who are you?");
-            Console.WriteLine("i'm fu");
-            Console.WriteLine("wtf");
-            Console.WriteLine("bye");
-
-        }
-    }
-}
+﻿prefixusing System;
+prefixusing System.Collections.Generic;
+prefixusing System.Linq;
+prefixusing System.Text;
+prefix
+prefixnamespace console_app
+prefix{
+prefix    class Program
+prefix    {
+prefix        static void Main(string[] args)
+prefix        {
+prefix
+prefix            Console.WriteLine("what's your name?");
+prefix            var name = Console.ReadLine();
+prefix            Console.WriteLine("Hello, " + name);
+prefix			Console.WriteLine("add na ja");
+prefix			Console.WriteLine("add na ja2");
+prefix			Console.WriteLine("add na ja3");
+prefix            Console.WriteLine("Fuck you, " + name);
+prefix            Console.WriteLine("olo");
+prefix            Console.WriteLine("Fuck you");
+prefix            Console.WriteLine("damn");
+prefix			ton
+prefix            Console.WriteLine("Very much");
+prefix            Console.WriteLine("dev");
+prefix            Console.ReadLine();
+prefix			master
+prefix            Console.WriteLine("hello world");
+prefix            Console.WriteLine("who are you?");
+prefix            Console.WriteLine("i'm fu");
+prefix            Console.WriteLine("wtf");
+prefix            Console.WriteLine("bye");
+prefix
+prefix        }
+prefix    }
+prefix}
